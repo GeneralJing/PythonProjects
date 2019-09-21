3b70b6c5fb3456f0e883b68a26eb64ad34fdf613
jingzhenyu <jingzhenyu@soyoung.com>
2019-09-21 23:58:36 +0800

    convolutions operation，detect_blur operation

--- OpenCV_Tutorial/convolutions.py
+++ OpenCV_Tutorial/convolutions.py
@@ -1,40 +1,3 @@
-from skimage.exposure import rescale_intensity
-import numpy as np
-import imutils
-import cv2
-import argparse
-
-def convolve(image, kernel):
-    (iH, iW) = image.shape[:2]
-    (kH, kW) = kernel.shape[:2]
-
-    pad = (kW-1)//2
-    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
-    output = np.zeros((iH, iW), dtype="float32")
-
-    for y in np.arange(pad, iH+pad):
-        for x in np.arange(pad, iW+pad):
-            roi = image[y-pad:y+pad+1, x-pad:x+pad+1]
-            k = (roi * kernel).sum()
-            output[y-pad, x-pad] = k
-
-    output = rescale_intensity(output, in_range=(0, 255))
-    output = (output * 255).astype("uint8")
-
-    return output
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to the image file")
-args = vars(ap.parse_args())
-
-smallBlur = np.ones((7, 7), dtype="float") * (1.0/(7*7))
-largeBlur = np.ones((21, 21), dtype="float") * (1.0/(21*21))
-
-sharpen = np.array(
-    [0, -1, 0],
-    [-1, 5, -1],
-    [0, -1, 0], dtype="int"
-)
 
 # import the necessary packages
 from skimage.exposure import rescale_intensity
@@ -112,10 +75,11 @@ sobelX = np.array((
 	[-1, 0, 1]), dtype="int")
 
 # construct the Sobel y-axis kernel
-sobelY = np.array((
-	[-1, -2, -1],
-	[0, 0, 0],
-	[1, 2, 1]), dtype="int")
+sobelY = np.array(
+	(
+		[-1, -2, -1],
+		[0, 0, 0],
+		[1, 2, 1]), dtype="int")
 
 kernelBank = (
     ("small_blur", smallBlur),
@@ -123,3 +87,18 @@ kernelBank = (
     ("sharpen", sharpen),
     ("laplacian", laplacian),
 )
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+for (kernelname, kernel) in kernelBank:
+	print("[INFO] applying {} kernel".format(kernelname))
+	convolveOutput = convolve(gray, kernel)
+	opencvOutput = cv2.filter2D(gray, -1, kernel)
+
+	cv2.imshow("original", gray)
+	cv2.imshow("{} - convolve".format(kernelname), convolveOutput)
+	cv2.imshow("{} - opencv".format(kernelname), opencvOutput)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
