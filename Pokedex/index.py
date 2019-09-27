90ca6fa453a823df99a859ef0a308a7a5340c908
jingzhenyu <jingzhenyu@soyoung.com>
2019-09-27 21:27:31 +0800

    pokedex index

--- Pokedex/index.py
+++ Pokedex/index.py
@@ -18,3 +18,20 @@ for spritePath in list_images(args["sprites"]):
     pokemon = spritePath[spritePath.rfind("/") + 1:].replace(".png", "")
     image = cv2.imread(spritePath)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.copyMakeBorder(image, 15, 15, 15, 15, cv2.BORDER_CONSTANT, value=255)
+
+    thresh = cv2.bitwise_not(image)
+    thresh[thresh>0] = 255
+
+    outline = np.zeros(image.shape, dtype="uint8")
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTRNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+    cv2.drawContours(outline, [cnts], -1, 255, -1)
+
+    moments = desc.describe(outline)
+    index[pokemon] = moments
+
+    f = open(args["index"], "wb")
+    f.write(pickle.dump(index))
+    f.close()
