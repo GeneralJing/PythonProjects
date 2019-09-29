0ebfc9e71b1f4b8df5d915953ca1e62267bafd5f
jingzhenyu <jingzhenyu@soyoung.com>
2019-09-29 19:50:34 +0800

    mobiledocscanner+perspective

---
+++ MobileDocScanner/transform.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmax(diff)]
+    rect[2] = pts[np.argmin(diff)]
+
+    return rect
+
