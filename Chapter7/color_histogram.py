97fcad790263894ae6c8cf1f5e4c8f321eeac67f
Jing <jingzhenyu@soyoung.com>
2019-10-12 18:29:30 +0800

    chapter7

--- Chapter7/color_histogram.py
+++ Chapter7/color_histogram.py
@@ -24,3 +24,30 @@ for (chan, color) in zip(chans, colors):
 
 plt.show()
 cv2.waitKey(0)
+
+fig = plt.figure()
+
+ax = fig.add_subplot(131)
+hist = cv2.calcHist([chans[1], chans[0]], [0, 1], None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation="nearest")
+ax.set_title("2D color histogram for g and b")
+plt.colorbar(p)
+
+ax = fig.add_subplot(132)
+hist = cv2.calcHist([chans[1], chans[2]], [0, 1], None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation="nearest")
+ax.set_title("2D color histogram for g and r")
+plt.colorbar(p)
+
+ax = fig.add_subplot(133)
+hist = cv2.calcHist([chans[0], chans[2]], [0, 1], None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation="nearest")
+ax.set_title("2D color histogram for b and r")
+plt.colorbar(p)
+
+print ("2D histogram shape:{}, with {} values".format(hist.shape, hist.flatten().shape[0]))
+
+hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+print ("3D histogram shape:{}, with {} values".format(hist.shape, hist.flatten().shape[0]))
+
+plt.show()
