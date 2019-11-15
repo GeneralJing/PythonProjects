3b15a70c708ac707bfdf2747240099570804f256
GeneralJing <GeneralJing3024@163.com>
2019-11-16 07:20:40 +0800

    eyetracker

--- EyeTracking/eyetracking.py
+++ EyeTracking/eyetracking.py
@@ -20,3 +20,14 @@ while True:
     (grabbed, frame) = camera.read()
     if args.get("video") and not grabbed:
         break
+    frame = imutils.resize(frame, width=300)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = et.track(gray)
+    for rect in rects:
+        cv2.rectangle(frame, (rect[0], rect[1]), (rect[2], rect[3]), (0, 255, 0), 2)
+    cv2.imshow("Tracking", frame)
+    if cv2.waitKey(1) & 0xFF==ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
