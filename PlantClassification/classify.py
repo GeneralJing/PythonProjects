82d43d469dc3fa3f2778dd5a6d61cc1774dd7ad6
GeneralJing <GeneralJing3024@163.com>
2019-11-19 08:05:33 +0800

    plantClassification done

--- PlantClassification/classify.py
+++ PlantClassification/classify.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 from pyimagesearch.rgbhistogram import RGBHistogram
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
+# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import numpy as np
 import argparse
@@ -36,7 +37,7 @@ le = LabelEncoder()
 target = le.fit_transform(target)
 
 (trainData, testData, trainTarget, testTarget) = train_test_split(data, target, test_size=0.3, random_state=42)
-model = RandomForestClassifier(n_estimatos=25, random_state=84)
+model = RandomForestClassifier(n_estimators=25, random_state=84)
 
 model.fit(trainData, trainTarget)
 print(classification_report(testTarget, model.predict(testData), target_names=targetNames))
@@ -48,7 +49,7 @@ for i in np.random.choice(np.arange(0, len(imagePaths)), 10):
     mask = cv2.imread(maskPath)
     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     features = desc.describe(image, mask)
-    flower = le.reverse_transform(model.predict(features))[0]
+    flower = le.inverse_transform(model.predict([features]))[0]
     print(imagePath)
     print("I think this flower is a {}".format(flower.upper()))
     cv2.imshow("image", image)
